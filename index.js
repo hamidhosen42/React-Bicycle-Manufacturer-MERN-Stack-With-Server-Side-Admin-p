483010b22598c6113daceeb2a7568fd17c637c3a
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-05-26 14:19:08 +0600

    addfie

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+var nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
